70eb0366f3074d6d38a32c7799f15c5b6efd029f
handyo <handy9420@gamil.com>
2022-06-23 18:37:20 +0900

    deploy gh-pages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+    reactStrictMode: true,
 }
 
-module.exports = nextConfig
+module.exports = {
+    nextConfig,
+    images: {
+        loader: 'akamai',
+        path: '/',
+    },
+    assetPrefix:
+        process.env.NODE_ENV === "production"
+            ? "https://boramyy.github.io/invitation"
+            : "",
+
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
 import '../styles/globals.css'
+import {seoContextProvider} from "../context/context";
+import {prefix} from "../config/config";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({Component, pageProps}) {
+    return (
+        <seoContextProvider value={{prefix}}>
+            <Component {...pageProps} />
+        </seoContextProvider>
+    )
 }
 
 export default MyApp
